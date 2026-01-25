33a4e9779f9bf1ef4828ecbecff8500c26901314
HikmatullohDev <tilmiz@list.ru>
2026-01-26 02:37:25 +0700

    homework-15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import "./training";
+import { Color } from '../enum/Color';
+import { Collection, nameCollection, numberCollection } from './collection';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,33 @@ import "./training";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
+
 export class AppComponent {
 
+  companyName: string = 'румтибет';
+  companyCategory: string = 'туризм';
+
+  constructor() {
+    this.saveLastVisit();
+    this.updateVisitsCount();
+  }
+
+  isPrimaryColor(color: Color): boolean {
+    const primaryColors: Color[] = [Color.RED, Color.GREEN, Color.BLUE];
+    return primaryColors.includes(color)
+  }
+
+  saveLastVisit(): void {
+    const now: string = new Date().toString();
+    localStorage.setItem('lastVisitDate', now);
+  }
+
+  updateVisitsCount(): void {
+    const storedValue = Number(localStorage.getItem('visitsCount') || '0');
+    localStorage.setItem('visitsCount', String(storedValue + 1));
+  }
+
 }
+
+nameCollection.getAll();
+numberCollection.replace(1, 77);
