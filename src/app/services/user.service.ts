c65fb9c07ac0efd9c1f2daaacb8098d5e2ce1dfd
HikmatullohDev <tilmiz@list.ru>
2026-06-17 04:00:46 +0700

    usersFilter component, @input @output

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -5,6 +5,7 @@ import { LoaderService } from './loader.service';
 import { MessageService } from './message.service';
 import { UserApiService } from './user-api.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
   providedIn: 'root',
@@ -14,21 +15,39 @@ export class UserService {
   private userApiService: UserApiService = inject(UserApiService);
   private loaderService: LoaderService = inject(LoaderService);
   private messageService: MessageService = inject(MessageService);
+  private localStorageService: LocalStorageService = inject(LocalStorageService);
 
   private usersSubject: BehaviorSubject<IUser[]> = new BehaviorSubject<IUser[]>([]);
   users$: Observable<IUser[]> = this.usersSubject.asObservable();
 
   setUsers(users: IUser[]): void {
     this.usersSubject.next(users);
+    this.localStorageService.setValue('users', users)
   }
 
   getUsers(): IUser[] {
     return this.usersSubject.getValue();
   }
 
-  loadUsers(): Observable<IUser[]> {
-    this.loaderService.showLoader();
+  deleteUser(user: IUser): void {
+    const updatedUsers = this.getUsers().filter(u => u.id !== user.id);
+    this.setUsers(updatedUsers);
+  }
 
+  addUser(user: IUser): void {
+    const currentUsers = this.getUsers();
+    this.setUsers([...currentUsers, user]); 
+  }
+
+  loadUsers(): Observable<IUser[]> {
+
+    const usersListStorage = this.localStorageService.getValue<IUser[]>('users');
+
+    if (usersListStorage && usersListStorage.length > 0) {
+      return of(usersListStorage);
+    }
+
+    this.loaderService.showLoader();
     return this.userApiService.getUsers()
     .pipe(
       catchError(() => {
---
+++ src/app/user-create/user-create.component.ts
@@ -0,0 +1,51 @@
+import { Component, inject, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IUser } from '../../interfaces/IUser';
+import { MessageService } from '../services/message.service';
+
+@Component({
+  selector: 'app-user-create',
+  imports: [ReactiveFormsModule],
+  templateUrl: './user-create.component.html',
+  styleUrl: './user-create.component.scss',
+})
+export class UserCreateComponent {
+
+  @Output() createUser: EventEmitter<IUser> = new EventEmitter<IUser>();
+
+  private fb: FormBuilder = inject(FormBuilder);
+  private messageService: MessageService = inject(MessageService);
+
+  userForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(100)]],
+    username: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
+    email: ['', [Validators.required, Validators.email, Validators.maxLength(100)]],
+    phone: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(25)]],
+    website: ['', [Validators.maxLength(100)]],
+    address: this.fb.group({
+      city: ['', [Validators.required, Validators.maxLength(50)]],
+      street: ['', [Validators.required, Validators.maxLength(100)]],
+      suite: ['', [Validators.maxLength(50)]],
+      zipcode: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]],
+      geo: this.fb.group({
+        lat: ['', [Validators.required]],
+        lng: ['', [Validators.required]],
+      })
+    }),
+    company: this.fb.group({
+      name: ['', [Validators.required, Validators.maxLength(50)]],
+      catchPhrase: ['', [Validators.maxLength(200)]],
+      bs: ['', [Validators.maxLength(100)]]
+    })
+  });
+
+  onSubmit(): void {
+    if (this.userForm.invalid) 
+      return this.messageService.showError('Не все поля верно заполнены!');
+
+    const newUser: IUser = {...this.userForm.getRawValue(), id: Date.now()};
+    this.createUser.emit(newUser);
+    this.userForm.reset();
+  }
+
+}
