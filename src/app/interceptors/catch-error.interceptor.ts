6d60d3f5d14b42617f55d0d036e99fcf988461a4
HikmatullohDev <tilmiz@list.ru>
2026-07-21 22:33:44 +0700

    fix code logic

--- src/app/interceptors/catch-error.interceptor.ts
+++ src/app/interceptors/catch-error.interceptor.ts
@@ -1,17 +1,17 @@
-import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { catchError} from 'rxjs';
+import { catchError, throwError} from 'rxjs';
 import { MessageService } from '../services/message.service';
 
 export const catchErrorInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
   const messageService: MessageService = inject(MessageService);
 
   return next(req).pipe(
-    catchError((error) => {
+    catchError((error: HttpErrorResponse) => {
       if (error.status >= 500) {
         messageService.showError(`Ошибка: ${ error.status }`);
       }
-      return [];
+      return throwError(() => error);;
     })
   );
 };
--- src/app/interceptors/logging.interceptor.ts
+++ src/app/interceptors/logging.interceptor.ts
@@ -1,15 +1,31 @@
-import { HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpRequest, HttpResponse } from '@angular/common/http';
-import { tap } from 'rxjs';
+import { HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpRequest, HttpResponse } from '@angular/common/http'; 
+import { tap } from 'rxjs'; 
 
-export const loggingInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
-  const startTime: number = Date.now();
-
-  return next(req).pipe(
-    tap((event: HttpEvent<unknown>) => {
-      if (event instanceof HttpResponse) {
-        const duration: number = Date.now() - startTime
-        console.log('request', { method: req.method, url: req.url, status: event.status, responseTime: duration })
+export const loggingInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) => { 
+  const startTime: number = Date.now(); 
+  
+  return next(req).pipe( 
+    tap({
+      next: (event: HttpEvent<unknown>) => { 
+        if (event instanceof HttpResponse) { 
+          const duration: number = Date.now() - startTime; 
+          console.log('HTTP request', { 
+            method: req.method, 
+            url: req.url, 
+            status: event.status, 
+            responseTime: duration 
+          }); 
+        } 
+      },
+      error: (error: HttpErrorResponse) => { 
+        const duration: number = Date.now() - startTime; 
+        console.log('HTTP error', { 
+          method: req.method, 
+          url: req.url, 
+          status: error.status, 
+          responseTime: duration 
+        }); 
       }
     })
-  )
+  ); 
 };
