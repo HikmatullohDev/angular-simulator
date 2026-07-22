64c2eba94dbf77a7c9a7e4b172a0123e1209afa2
HikmatullohDev <tilmiz@list.ru>
2026-07-22 20:04:55 +0700

    fix code format

--- src/app/interceptors/logging.interceptor.ts
+++ src/app/interceptors/logging.interceptor.ts
@@ -2,29 +2,26 @@ import { HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpReq
 import { tap } from 'rxjs'; 
 
 export const loggingInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) => { 
-  const startTime: number = Date.now(); 
-  
-  return next(req).pipe( 
+  const startTime: number = Date.now();
+
+  const logRequest = (result: 'request' | 'error', status: number) => {
+    console.log(`HTTP ${result}`, {
+      method: req.method,
+      url: req.url,
+      status: status,
+      responseTime: Date.now() - startTime
+    });
+  }
+
+  return next(req).pipe(
     tap({
       next: (event: HttpEvent<unknown>) => { 
         if (event instanceof HttpResponse) { 
-          const duration: number = Date.now() - startTime; 
-          console.log('HTTP request', { 
-            method: req.method, 
-            url: req.url, 
-            status: event.status, 
-            responseTime: duration 
-          }); 
+          logRequest('request', event.status);
         } 
       },
       error: (error: HttpErrorResponse) => { 
-        const duration: number = Date.now() - startTime; 
-        console.log('HTTP error', { 
-          method: req.method, 
-          url: req.url, 
-          status: error.status, 
-          responseTime: duration 
-        }); 
+        logRequest('error', error.status);
       }
     })
   ); 
